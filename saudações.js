7bf55a8550badc8b48efbd5bb420038379089aae
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-30 20:54:32 -0300

    update saudações.js

--- saudações.js
+++ saudações.js
@@ -1,4 +1,5 @@
 let oi = "Como você está?"
 
-console.log("Olá, bom dia! " + saudacao);
-console.log("Olá, boa tarde! " + 
+console.log("Olá, bom dia! " + oi);
+console.log("Olá, boa tarde! " + oi);
+console 
