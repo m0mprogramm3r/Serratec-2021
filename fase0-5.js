ad94d6dc72fe867fcfbebfb4cd12d28b74a63dbc
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 01:00:22 -0300

    rename saudaçãopuxasaco.js

---
+++ fase0-5.js
@@ -0,0 +1,9 @@
+let dia = "Olá, bom dia, "
+let tarde = "boa tarde, "
+let noite = "boa noite!"
+
+console.log("Segunda-feira: " + dia + tarde + noite);
+console.log("Terça-feira: " + dia + tarde + noite);
+console.log("Quarta-feira: " + dia + tarde + noite);
+console.log("Quinta-feira: " + dia + tarde + noite);
+console.log("Sexta-feira: " + dia + tarde + noite);
