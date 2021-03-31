244265355d809170a5776406a26076d088f071c9
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 13:37:41 -0300

    update fase3-11-12.js

--- fase3-11-12.js
+++ fase3-11-12.js
@@ -6,4 +6,4 @@ let x3 = soma / 3
 console.log("Alice, você pode parcelar em 3 vezes sem juros. As parcelas ficam em R$" + x3 + " reais!");
 
 let x5 = soma / 5
-console.log("Você também tem a opção de parcelar em 5 vezes sem jueos
+console.log("Você também tem a opção de parcelar em 5 vezes sem juros. As parcelas ficam em R$" + x5 + " reais!");
