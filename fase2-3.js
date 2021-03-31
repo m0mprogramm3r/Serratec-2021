e8c52d1f74eb281bd0861e927d15bb6765b235c8
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 13:14:29 -0300

    update fase2-3.js

--- fase2-3.js
+++ fase2-3.js
@@ -1,5 +1,8 @@
 let sorte 
-let sorte2 
+let sorte2 = 22
 
 sorte = 13
-console.log("Quero te contar meu bumero
+console.log("Quero te contar meu número da sorte. É " + sorte + " !");
+
+sorte = 7 
+console.lof
