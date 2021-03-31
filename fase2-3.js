faa5283ead3237bd0a4952d48d84a80f94beb31d
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 13:10:40 -0300

    update fase2-3.js

--- fase2-3.js
+++ fase2-3.js
@@ -8,4 +8,4 @@ let sorte2 = 22
 console.log("Não, espera. É " + sorte + "... Ah, esse era o da mamãe. O meu é " + sorte2 + " !");
 
 sorte = 20
-console.log("Já decidi! É " + sorte + " !. BINGO :
+console.log("Já decidi! É " + sorte + " !. BINGO :)");
