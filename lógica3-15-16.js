87021f3e5b8a1dde09a6b32e21c750c1506a6e06
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:27:45 -0300

    update lógica3-15-16.js

--- lógica3-15-16.js
+++ lógica3-15-16.js
@@ -1,2 +1,4 @@
 //Programa Fidelidade
-//O programa fidelidade solicitou a criação do sistema "ver meus pontos", onde o cliente consulta seu saldo seu saldo de pontos 
+//O programa fidelidade solicitou a criação do sistema "ver meus pontos", onde o cliente consulta seu saldo seu saldo de pontos.
+//O cliente digite um número inteiro correspondente aos seus cupons. 
+//Cada cupom vale 1 ponto.
