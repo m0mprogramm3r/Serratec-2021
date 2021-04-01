da072a68843ba9870ab1c7ba6ec7f28700a1391a
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 23:09:44 -0300

    rename fase3-2.js

---
+++ lógica3-2.js
@@ -0,0 +1,12 @@
+//Soma das idades de todos do squad
+//leks de konoha
+//Alice: 18, Giovanne: 27, Giulia: 22, Larissa: 23, Taiana: 26.
+
+let lili = 18;
+let gio = 27;
+let giu = 22;
+let lari = 23;
+let tai = 26;
+
+let squad = lili + gio + giu + lari + tai;
+console.log("A soma das idades dos integrantes do nosso squad 'leks de konoha' é " + squad + " ! :)");
