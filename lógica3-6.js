39cfe2314eea5cd2d344fa7108a63d4539b09df8
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 23:26:46 -0300

    update lógica3-6.js

--- lógica3-6.js
+++ lógica3-6.js
@@ -1,3 +1,6 @@
 //Total de produtos 
 //Irá solicitar que o cliente digite qauntos produtos irá comprar.
-//Depois exibe a mensagem: "Parabéns por comprar x produtos!" 
+//Depois exibe a mensagem: "Parabéns por comprar x produtos!", substituindo x pela quantidade.
+
+const prompt = require("prompt-sync")();
+let x = prompt("
