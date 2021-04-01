90d788f547c758f7d7816d2479f4591d31be7ac7
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 21:45:59 -0300

    update fase3-2.js

--- fase3-2.js
+++ fase3-2.js
@@ -1,6 +1,10 @@
 //leks de konoha
 //Alice: 18, Giovanne: 27, Giulia: 22, Larissa: 23, Taiana: 26.
 
-let lili = 18
-let gio = 27
+let lili = 18;
+let gio = 27;
 let giu = 22;
+let lari = 23;
+let tai = 26;
+
+let squad
