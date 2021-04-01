8828e7d31d4e60a5ba2768458cdcac7088319b78
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 23:10:12 -0300

    rename fase3-4.js

--- fase3-4.js
+++
@@ -1,9 +0,0 @@
-//O programa Termômetro lê em Celsius e devolve sua equivalência na escala Fahrenheit.
-
-const prompt = require("prompt-sync")();
-
-let celsius = prompt("Qual temperatura em Celsius você quer converter? Digite aqui: ");
-
-let fahr = (celsius * 9 / 5) + 32
-console.log("Boa escolha!");
-console.log("A temperatura escolhida em Celsius, é: " + fahr + " graus fahrenheit!");
