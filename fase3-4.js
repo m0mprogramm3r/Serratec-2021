7c2be5af2421187f2f839dca453bbf0908a647eb
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 21:57:09 -0300

    update fase3-4.js

--- fase3-4.js
+++ fase3-4.js
@@ -1,3 +1,6 @@
 const prompt = require("prompt-sync")();
 
 let celsius = prompt("Qual temperatura em Celsius você quer converter? Digite aqui: ");
+
+let fahr = (celsius * 9 / 5) + 32
+console.log(
