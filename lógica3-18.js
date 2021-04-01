a7017c378800db83177b7c09081895c81b435a07
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:59:20 -0300

    update lógica3-18.js

---
+++ lógica3-18.js
@@ -0,0 +1,10 @@
+//O menino, os tênis e os apitos 
+
+let shoes = 10
+let boy = 5
+let apitos = 4 
+let sapato = shoes / 2
+let apito = apitos / 2 
+let res = sapato + boy * apito
+
+console.log("Sapato + menino x apito = " + res);
