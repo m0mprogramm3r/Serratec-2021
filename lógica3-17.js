d49a74872540e874a0f176c9a94ca929e6da6d42
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:43:48 -0300

    update lógica3-17.js

--- lógica3-17.js
+++ lógica3-17.js
@@ -1,3 +1,6 @@
 //Laboratório de álcool em gel
 //O laboratório fabricante precisa saber se o produto está atingindo a concentração ideal para desinfecção, que é de 70%.
-//Crie um programa onde será preenchida a capacidade da garrafa em mililitros, e o resultado será os volumes de álcool e de gel que 
+//Crie um programa onde será preenchida a capacidade da garrafa em mililitros, e o resultado será os volumes de álcool e de gel que precisam ser misturados para o preenchimento do vasilhame.
+
+const prompt = require(prompt-sync")(); 
+let garrafa = 
