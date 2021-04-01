4ad0592bbb086e85754c76e5e8473a61d4484790
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 23:40:45 -0300

    update lógica3-7.js

--- lógica3-7.js
+++ lógica3-7.js
@@ -1,2 +1,6 @@
 //Mini-calculadora 
-//Após ter dois números inteiros, apresenta as operações de soma, subtração, mu
+//Após ter dois números inteiros, apresenta as operações de adição, subtração, multiplicação e divisão com eles.
+
+const prompt = require("prompt-sync")();
+
+let n1 = prompt
