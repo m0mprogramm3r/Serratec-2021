974f3c4f4ec5e4b9e0b725f4207dcf0996c2b93f
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 01:43:23 -0300

    update lógica3-10.js

--- lógica3-10.js
+++ lógica3-10.js
@@ -1,3 +1,5 @@
 //O dobro
 //Um programa que solicita um número, calcula o dobro do valor, e devolve a mensagem:
-//"O dobro de x foi y". Substi
+//"O dobro de x foi y". Substituindo x e y pelos valores.
+
+const prompt = require("prompt-sync")();
