406ba2ec0aa67c89b12f7979188fc8b2299c5ae0
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 21:37:39 -0300

    update fase3-1.js

--- fase3-1.js
+++ fase3-1.js
@@ -1,5 +1,5 @@
 let mul4 = 1004 + 1008 + 1012 + 1016;
 let nump = 2 + 3 + 5 + 7; 
-let results = mul4 - nump
+let results = mul4 - nump;
 
-console.log("O resultado da soma dos 4 primeiros múltiplos de 4 acima de mil, subtraindo dos 4 primeiros números primos a partir do zero, é: " + results + " !
+console.log("O resultado da soma dos 4 primeiros múltiplos de 4 acima de mil, subtraindo dos 4 primeiros números primos a partir do zero, é: " + results + " ! :)");
