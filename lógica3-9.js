71a0c2092d2136e462f7170c8c06bf46547ea64a
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:20:33 -0300

    update lógica3-9.js

--- lógica3-9.js
+++ lógica3-9.js
@@ -3,3 +3,8 @@
 //Solicite que seus amigos lhe fale um número entre 1 e 10, cada um.
 //Crie uma variável com cada número recebido e atribua nela os valores, digitando na seguinte ordem: os pares são somados, os ímpares subtraídos.
 
+let lili = 4
+let lari = 3
+let tai = 8 
+let gio = 7 
+let par = 
