2723f8f21e0e3701d1b4d2b8a7a80a7ee3d2a3ab
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:42:46 -0300

    update lógica3-17.js

--- lógica3-17.js
+++ lógica3-17.js
@@ -1,2 +1,3 @@
 //Laboratório de álcool em gel
-//O laboratório fabricante precisa saber se o produto está atingindo a concentração ideal para desinfecc
+//O laboratório fabricante precisa saber se o produto está atingindo a concentração ideal para desinfecção, que é de 70%.
+//Crie um programa onde será preenchida a capacidade da garrafa em mililitros, e o resultado será os volumes de álcool e de gel que 
