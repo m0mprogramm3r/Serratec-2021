3ba0a81bcf3dc8c956be2bb774263848d1ab7bb8
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 23:10:00 -0300

    rename fase3-3.js

--- fase3-3.js
+++
@@ -1,7 +0,0 @@
-//O programa Ryuju Okada mostra o resultado das expressões abaixo
-
-let a = 2 + 3 - 5 * 8 - 4 + 354 - 521 + 7 * 66 
-let b = 2 + 7 * (14 - 21) + 28 * 3 * 42 + 740 - (156 + 4 + 40) * 9
-
-console.log(a)
-console.log(b)
