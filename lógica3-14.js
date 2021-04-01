15353af93154f8d441c0e15446eee806d5594319
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:13:34 -0300

    update lógica3-14.js

--- lógica3-14.js
+++ lógica3-14.js
@@ -1,3 +1,7 @@
 //Jennyffer
 //Jennyffer está preparando as malas para passar o feriado prolongado na casa da avó.
 //Colocou na mala 4 calças e 8 camisetas.
+//Quantas combinações, ou, de quantas formas diferentez ela poderá usar essas roupas?
+
+let x = 4 
+let y = 
