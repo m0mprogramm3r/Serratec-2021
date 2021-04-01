d10fdf846f73b9269f996ced3b6ba5dcefaac065
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:28:50 -0300

    update lógica3-15-16.js

--- lógica3-15-16.js
+++ lógica3-15-16.js
@@ -2,3 +2,6 @@
 //O programa fidelidade solicitou a criação do sistema "ver meus pontos", onde o cliente consulta seu saldo seu saldo de pontos.
 //O cliente digite um número inteiro correspondente aos seus cupons. 
 //Cada cupom vale 1 ponto.
+
+const prompt = require("prompt-sync")(); 
+let cupons = prompt("
