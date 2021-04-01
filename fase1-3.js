f3235add1b12c392d80c3eb7fff8a51ddaa31b32
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 23:08:38 -0300

    rename fase1-3.js

--- fase1-3.js
+++
@@ -1,7 +0,0 @@
-//Escrever 3 mensagens diferentes e separadas no console, com um único comando de escrita.
-
-let puxa1 = "Hello" 
-let puxa2 = "world"
-let puxa3 = ":)"
-
-console.log(puxa1 + "\n" + puxa2 + "\n" + puxa3)
