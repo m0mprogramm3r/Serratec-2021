e6604e71c9fa751456bef15ead379649ebb47e0b
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 01:30:32 -0300

    update lógica3-8.js

--- lógica3-8.js
+++ lógica3-8.js
@@ -1,5 +1,9 @@
 //Faço Parte 
 //Te mostra um histórico da trajetória da nossa cidade.
 //O resultado apresentado na tela será: "Teresópolis tem x anos. Desses, foram y antes de mim. Mas nos últimos z anos ela conta comigo."
-//Cada letra maiuscula do texro será uma variável, sendo que x e z são valores fixos, e y irá conter uma expressão.
+//Cada letra maiuscula do texto será uma variável, sendo que x e z são valores fixos, e y irá conter uma expressão.
+
+const x = 129
+const y = 22
+
 
