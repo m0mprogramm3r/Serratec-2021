8c9b50d43b4c73d3f369c1a2e2554b14b65cd4e5
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:19:16 -0300

    update lógica3-9.js

--- lógica3-9.js
+++ lógica3-9.js
@@ -1,3 +1,5 @@
 //Betmais
 //O sistema organiza as apostas dos seus amigos.
-//Solicite que seus amigos lhe fale um número entre 1
+//Solicite que seus amigos lhe fale um número entre 1 e 10, cada um.
+//Crie uma variável com cada número recebido e atribua nela os valores, digitando na seguinte ordem: os pares são somados, os ímpares subtraídos.
+
