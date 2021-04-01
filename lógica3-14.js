af2fdde4629c14f1c480b5dd51349429369927c5
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-01 02:15:04 -0300

    update lógica3-14.js

--- lógica3-14.js
+++ lógica3-14.js
@@ -6,4 +6,4 @@
 let x = 4 
 let y = 8
 let z = 4 * 8
-console.log("Jennyffer poderá usar as roupas de " + z + " combinações di
+console.log("Jennyffer poderá usar as roupas de " + z + " combinações diferentes e assim aproveitar bastante esse tempo com a avó!");
