46e020ee7523c3e548ef338d85e0b701ea5698ed
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-03-31 23:07:39 -0300

    rename fase0-4.js

--- fase0-4.js
+++
@@ -1,7 +0,0 @@
-//Saudações, com três linhas de mensagem, uma para cada cumprimento.
-
-let oi = "Como você está?"
-
-console.log("Olá, bom dia! " + oi);
-console.log("Olá, boa tarde! " + oi);
-console.log("Olá, boa noite! " + oi);
