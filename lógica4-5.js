0368b77fc51c70e242d1fcb4ef42a2b348e85dd3
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 20:27:15 -0300

    update lógica4-5.js

--- lógica4-5.js
+++ lógica4-5.js
@@ -1,2 +1,3 @@
 //Self-service
-//O restaurante precisa de um programa para imprimir as etiquetas da "comanda" 
+//O restaurante precisa de um programa para imprimir as etiquetas da "comanda" na pesagem dos pratos. 
+//O operador da balança irá digitar o preço do quilo  
