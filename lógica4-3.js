db6e52d43e723a733029625176b252a65495fee2
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 19:35:23 -0300

    update lógica4-3.js

---
+++ lógica4-3.js
@@ -0,0 +1,7 @@
+//Homem-Aranha
+//"Com grandes poderes, vêm grandes responsabilidades.
+//Irá criar essa frase fazendo uma atribuição nova para cada caractere.
+//Cada letra será atribuída a uma nova linha.
+//Caso use o Unicode, não precisa fazer uma atribuição por linha, basta separar cada palavra em uma variável.
+
+let Com = "\u0043\u
