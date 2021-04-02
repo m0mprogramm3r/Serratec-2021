65e90602666f1c0b135bc46a5c6472eb574441a6
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 20:07:26 -0300

    update lógica4-4.js

--- lógica4-4.js
+++ lógica4-4.js
@@ -1,2 +1,3 @@
 //Financeira
-//O programa irá solicitar que o cliente digite seu nome, empresa e função, além do valor do empréstimo  
+//O programa irá solicitar que o cliente digite seu nome, empresa e função, além do valor do empréstimo que deseja contratar. 
+//Então será calculado 
