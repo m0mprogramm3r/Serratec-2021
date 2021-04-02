f6f4edc6296ae2477d89ab49e27e6c1144baea1d
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 20:32:19 -0300

    update lógica4-5.js

--- lógica4-5.js
+++ lógica4-5.js
@@ -1,3 +1,4 @@
 //Self-service
 //O restaurante precisa de um programa para imprimir as etiquetas da "comanda" na pesagem dos pratos. 
 //O operador da balança irá digitar o preço do quilo e o total em gramas da refeição, considerando que o prato vazio pesa 465g(tara).
+//
