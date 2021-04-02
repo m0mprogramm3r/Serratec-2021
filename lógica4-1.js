498217aac68a5c4bfc38f49599d3636a0bd303b9
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 19:02:49 -0300

    update lógica4-1.js

--- lógica4-1.js
+++ lógica4-1.js
@@ -1,3 +1,5 @@
 //Educado
 //O programa irá perguntar seu nome e como gostaria de ser chamado. 
-//Depois disso, uma saudadao l
+//Depois disso, uma saudação para você aparece na tela. 
+
+const prompt = require(prompt-sync
