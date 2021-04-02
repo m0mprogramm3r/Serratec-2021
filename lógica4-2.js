19e312f4240999096d26b7fc85833cdb90ebd761
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 19:10:32 -0300

    update lógica4-2.js

--- lógica4-2.js
+++ lógica4-2.js
@@ -6,4 +6,4 @@ const prompt = require("prompt-sync")();
 let nome = prompt("Diga-me seu nome: ");
 let sobrenome = prompt("E agora, o seu sobrenome: ");
 
-console.log("Olá " + nome + sobrenome + 
+console.log("Olá " + nome + sobrenome + " seja bem-vindo ao universo da programação!");
