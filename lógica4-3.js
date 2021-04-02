de693128944c495ce02db257e6f6c70eac7c1009
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 19:53:54 -0300

    update lógica4-3.js

--- lógica4-3.js
+++ lógica4-3.js
@@ -4,10 +4,10 @@
 //Cada letra será atribuída a uma nova linha.
 //Caso use o Unicode, não precisa fazer uma atribuição por linha, basta separar cada palavra em uma variável.
 
-let com = "\u0043\u006F\u006D";
-let grandes = "\u0067\u0072\u0061\u006E\u0064\u0065\u0073";
-let poderes = "\u0070\u006F\u0064\u0065\u0072\u0065\u0073";
-let vem = "\u0076\u00EA\u006D";
+let com = "\u0043\u006F\u006D ";
+let grandes = "\u0067\u0072\u0061\u006E\u0064\u0065\u0073 ";
+let poderes = "\u0070\u006F\u0064\u0065\u0072\u0065\u0073 ";
+let vem = "\u0076\u00EA\u006D ";
 let responsabilidades = "\u0072\u0065\u0073\u0070\u006F\u006E\u0073\u0061\u0062\u0069\u006C\u0069\u0064\u0061\u0064\u0065\u0073";
 
 console.log(com + grandes + poderes + vem + grandes + responsabilidades + " !");
