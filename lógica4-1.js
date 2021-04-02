2b0a96a9f69bb7b4f7aa354a508b2f554b022862
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 19:03:51 -0300

    update lógica4-1.js

--- lógica4-1.js
+++ lógica4-1.js
@@ -2,4 +2,7 @@
 //O programa irá perguntar seu nome e como gostaria de ser chamado. 
 //Depois disso, uma saudação para você aparece na tela. 
 
-const prompt = require(prompt-sync
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Olá, qual seu nome?: ");
+let sobrenome = prompt
