4b5a7befccdf199e5694404a1ba40ca0de046a98
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-02 20:10:46 -0300

    update lógica4-4.js

--- lógica4-4.js
+++ lógica4-4.js
@@ -2,4 +2,4 @@
 //O programa irá solicitar que o cliente digite seu nome, empresa e função, além do valor do empréstimo que deseja contratar. 
 //Então será calculado 86% do valor desejado como crédito disponível.
 //A resposta deverá seguir o seguinte exemplo:
-//Senhor Moisés, você está com sorte! Pela sua função de programador, e sua excelente relação com a empresa onde trabalha,
+//Senhor Moisés, você está com sorte! Pela sua função de programador, e sua excelente relação com a empresa onde trabalha, foi aprovado um crédito em conta corrente de 
