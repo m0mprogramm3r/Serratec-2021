a26c7065eaf60138bbb13f8c30616ea91ec80ac4
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-03 00:21:50 -0300

    update lógica4-6.js

--- lógica4-6.js
+++ lógica4-6.js
@@ -2,4 +2,4 @@
 //O programa lê a quantidade de horas trabalhadas por um funcionário em um mês, o valor que ele recebe por hora e o percentual de desconto para o INSS e calcula:
 //Salário bruto, valor do desconto para o INSS e o salário líquido.
 
-const 
+const ler = require("prompt-sync")();
