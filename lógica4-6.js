c3b95d75e835d5f6f768c03f86bc779b8cd9061e
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-03 00:21:28 -0300

    update lógica4-6.js

--- lógica4-6.js
+++ lógica4-6.js
@@ -1,2 +1,5 @@
 //Mini departamento pessoal
-//O programa lê a quantidade de horas trabalhadas por um funcionário em um mês, o valor que ele recebe por hora e o percentual 
+//O programa lê a quantidade de horas trabalhadas por um funcionário em um mês, o valor que ele recebe por hora e o percentual de desconto para o INSS e calcula:
+//Salário bruto, valor do desconto para o INSS e o salário líquido.
+
+const 
