525ad28107b8dc20b5320621ddef6380af915a52
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-03 00:05:51 -0300

    update lógica4-5.js

--- lógica4-5.js
+++ lógica4-5.js
@@ -3,7 +3,7 @@
 //O operador da balança irá digitar o preço do quilo e o total em gramas da refeição, considerando que o prato vazio pesa 465g(tara).
 //A etiqueta irá conter o nome do restaurante, a tara do prato, o preço de 100g, o peso consumido e o valor total.
 
-const prompt = require("prompt-sync")();
+const ler = require("prompt-sync")();
 
 let preco = (ler("Valor do quilo: ");
 let gramas = (ler("Total em gramas da refeição: ");
