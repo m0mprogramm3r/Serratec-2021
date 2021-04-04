2a70ba381810d5fa937384b7c18d5f0d53597cf2
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-04 12:42:16 -0300

    update lógica4-9.js

---
+++ lógica4-9.js
@@ -0,0 +1,10 @@
+let boolean = isTooSmall = false
+
+let a = 5; 
+let b = 7;
+
+boolean = a > b
+console.log("a é maior que b? " + boolean);
+
+boolean = a < b
+console.log("a é menor que b? " + boolean);
