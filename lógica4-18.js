466083b5cbb7867d74f0ff853aa8b27cd52963e3
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:47:17 -0300

    update lógica4-18.js

--- lógica4-18.js
+++ lógica4-18.js
@@ -2,14 +2,13 @@
 //O astrólogo que escreveu o sistema acredita que os nascidos sob o signo de Aquário são mais inteligentes que os demais.
 
 const ler = require("prompt-sync")();
-let boolean = isTooSmall = false;
 
 let mes = ler("Mês do seu nascimento: ");
-mes = Number(mes);
-let niver = ler("Dia do aniversário: ");
-niver = Number(niver);
+let dia = ler("Dia do aniversário: ");
 
-mes = mes <= 2;
-niver = (mes == 1, niver >= 21) && (mes == 2, niver <= 19);
+let inicio = mes == 1 && dia >= 21;
+let fim = mes == 2 && dia <= 19;
+
+let boolean = (mes == 1 && dia >= 21) || (mes == 2 && dia <= 19);
 
 console.log("Você é inteligente? " + boolean);
