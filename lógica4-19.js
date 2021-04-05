b1ea1226f7d297e93114ad1a5a8253d153774058
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:45:23 -0300

    update lógica4-19.js

--- lógica4-19.js
+++ lógica4-19.js
@@ -2,27 +2,23 @@
 //Ajude o banco de sangue a fazer a triagem dos voluntários que responderam "sim" para as restrições.
 
 const ler = require("prompt-sync")();
-let boolean = isTooSmall = false;
 
 console.log(" == Banco de Sangue ==");
-console.log("[Digite "Sim" ou "Não" nas restrições a seguir]");
+console.log("[Digite Sim ou Não nas restrições a seguir]");
 
 let idade = ler("Idade abaixo de 16 ou acima de 69 anos: ");
-idade = boolean(idade);
-
 let peso = ler("Pesa menos de 50kg: ");
-peso = boolean(peso);
-
 let hep = ler("Portador de hepatite: ");
-hep = boolean(hep);
-
 let mal = ler("Já teve malária: ");
-mal = boolean(mal);
-
 let doa = ler("Fez doação recente: ");
-doa = boolean(doa);
 
 console.log("[Resultado da triagem]");
 
-boolean = (idade, peso, hep, mal, doa == "não") == true;
+let res1 = idade ==  16 >= 69;
+let res2 = peso == peso >= 50;
+let res3 = hep == "nao";
+let res4 = mal == "nao";
+let res5 = doa == "nao";
+let boolean = (idade == "nao" && peso == "nao") && (hep == "nao" && mal == "nao" && doa == "nao");
+
 console.log("Pode doar sangue? " + boolean);
