b63c7f2360bb3bdaa235ae3e062920614e5fd91f
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:54:31 -0300

    update lógica4-1.js

--- lógica4-1.js
+++ lógica4-1.js
@@ -2,9 +2,9 @@
 //O programa irá perguntar seu nome e como gostaria de ser chamado. 
 //Depois disso, uma saudação para você aparece na tela. 
 
-const prompt = require("prompt-sync")();
+const ler = require("prompt-sync")();
 
-let nome = (ler("Olá, qual seu nome?: "));
-let sobrenome = prompt("Que bonito!! Agora me diga, qual seu sobrenome?: ");
+let nome = ler("Olá, qual seu nome?: ");
+let sobrenome = ler("Que bonito!! Agora me diga, qual seu sobrenome?: ");
 
-console.log("Tenha um excelente dia com muito sucesso, " + nome + sobrenome " !");
+console.log("Tenha um excelente dia com muito sucesso, " + nome + " " + sobrenome + " !");
