4710e5cd840277a737baff43d14adcb7fc60a962
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-04 22:55:05 -0300

    update lógica4-16.js

--- lógica4-16.js
+++ lógica4-16.js
@@ -14,7 +14,7 @@ nasc = Number(nasc);
 let atual = 2021;
 sexo = (sexo = "feminino"), (sexo = "masculino");
 nasc = atual - nasc
-nasc = (sexo = "feminino" == nasc >= 21) && (sexo = "masculino" == 
+nasc = (sexo = "feminino" == nasc >= 21) && (sexo = "masculino" == nasc >= 18);
 boolean == (sexo) && (nasc);
 
 console.log("Cidadão emancipado? " + boolean);
