99933638e927ffe976c95a62b7e756f7467dffa9
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-04 23:59:36 -0300

    update lógica4-19.js

--- lógica4-19.js
+++ lógica4-19.js
@@ -1,2 +1,4 @@
 //Banco de sangue
-//Ajude o banco de sangue a faz
+//Ajude o banco de sangue a fazer a triagem dos voluntários que responderam "sim" para as restrições.
+
+const ler = require("prompt-s
