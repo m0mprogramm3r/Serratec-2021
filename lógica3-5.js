1e690128901227089cb5410c0160bc4a22cf51e8
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:52:28 -0300

    update lógica3-5.js

--- lógica3-5.js
+++ lógica3-5.js
@@ -1,9 +1,9 @@
 //Continha
 //Lê um número e mostra seu valor negativo
 
-const prompt = require("prompt-sync")();
+const ler = require("prompt-sync")();
 
-let x = prompt("Digite um número: ");
+let x = ler("Digite um número: ");
 
 let neg = -x
 console.log(neg);
