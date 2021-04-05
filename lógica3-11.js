da5fb9dc3dd6391e0cdd549898a0d8c89817b440
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:50:00 -0300

    update lógica3-11.js

--- lógica3-11.js
+++ lógica3-11.js
@@ -1,9 +1,9 @@
 //Quadrado
 //O programa terá um código adaptado do item anterior, para calcular o quadrado do número digitado.
 
-const prompt = require("prompt-sync")();
+const ler = require("prompt-sync")();
 
-let x = prompt("Digite um número: ");
+let x = ler("Digite um número: ");
 let y = x * x
 
 console.log("O resultado do quadrado de " + x + " é " + y + " ! :)");
