8ef9bb3ebd1ff64d0ff9b3d39560efcc70962df3
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 00:36:28 -0300

    update lógica4-20.js

--- lógica4-20.js
+++ lógica4-20.js
@@ -1,6 +1,6 @@
 //Gincana
 //Na gincana de arrecadação de alimentos, as equipes A e B têm uma semana para formar um pilha com as doações.
-//Sendo que a empresa irá a quantidade de quilos de feijão arrecadados.
+//Sendo que a empresa irá dobrar a quantidade de quilos de feijão arrecadados.
 //Ganha um ponto extra a equipe cuja quantidade de pacotes permitir a montagem de kits com 3 alimentos.
 //O programa irá apurar as quantidades recolhidas e exibir o nome da equipe vencedora. 
 
