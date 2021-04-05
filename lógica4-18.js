80acd359ab71631e9895c7db59b48e72f3235ee1
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-04 23:31:18 -0300

    update lógica4-18.js

--- lógica4-18.js
+++ lógica4-18.js
@@ -1,4 +1,6 @@
 //Zodíaco
 //O astrólogo que escreveu o sistema acredita que os nascidos sob o signo de Aquário são mais inteligentes que os demais.
 
-const
+const ler = require("prompt-sync")();
+let boolean = isTooSmall = false;
+
