ec5607da824ffc597eaf5885c64ec323d17bcbab
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:49:22 -0300

    update lógica3-10.js

--- lógica3-10.js
+++ lógica3-10.js
@@ -2,9 +2,9 @@
 //Um programa que solicita um número, calcula o dobro do valor, e devolve a mensagem:
 //"O dobro de x foi y". Substituindo x e y pelos valores.
 
-const prompt = require("prompt-sync")();
+const ler = require("prompt-sync")();
 
-let x = prompt("Digite um número: ");
+let x = ler("Digite um número: ");
 let y = x * 2
 
 console.log("O número de " + x + " foi " + y + " ! :)");
