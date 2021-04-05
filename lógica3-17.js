0aedf6e04dd9839454718449dca0a6abb5e1dc6d
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:51:13 -0300

    update lógica3-17.js

--- lógica3-17.js
+++ lógica3-17.js
@@ -2,9 +2,9 @@
 //O laboratório fabricante precisa saber se o produto está atingindo a concentração ideal para desinfecção, que é de 70%.
 //Crie um programa onde será preenchida a capacidade da garrafa em mililitros, e o resultado será os volumes de álcool e de gel que precisam ser misturados para o preenchimento do vasilhame.
 
-const prompt = require(prompt-sync")(); 
-let garrafa = prompt ("Diga-me: quantos ml tem a garrafa? ");
+const ler = require("prompt-sync")(); 
+let garrafa = ler("Diga-me: quantos ml tem a garrafa? ");
 let alcool = garrafa * 70 / 100
 let gel = garrafa - alcool
 
-console.log("Para se fazer álcool em gel em uma garrafa de " + garrafa + "ml, é necessário " + alcool + "ml de álcool, e de " + gel + "ml de gel");
+console.log("Para se fazer álcool em gel em uma garrafa de " + garrafa + "ml, é necessário " + alcool + "ml de álcool, e de " + gel + "ml de gel!");
