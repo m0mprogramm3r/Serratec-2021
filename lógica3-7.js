053a7edaa33ed098f380b7461926ec06a0c0cc4b
Giulia Nascimento <79417886+m0mprogramm3r@users.noreply.github.com>
2021-04-05 03:53:54 -0300

    update lógica3-7.js

--- lógica3-7.js
+++ lógica3-7.js
@@ -1,13 +1,12 @@
 //Mini-calculadora 
 //Após ter dois números inteiros, apresenta as operações de adição, subtração, multiplicação e divisão com eles.
 
-const prompt = require("prompt-sync")();
+const ler = require("prompt-sync")();
 
-let n1 = prompt("Digite um número para começarmos: ");
-let n2 = prompt("Digite outro número: ");
+let n1 = parseInt(ler("Digite um número para começarmos: "));
+let n2 = parseInt(ler("Digite outro número: "));
 
-console.log("O resultado de " + n1 + " + " + n2 + " é " n1 + n2);
-console.log("O resultado de " + n1 + " - " + n2 + " é " n1 - n2);
-console.log("O resultado de " + n1 + " x " + n2 + " é " n1 * n2);
-console.log("O resultado de " + n1 + " ÷ " + n2 + " é " n1 / n2); 
-
+console.log("O resultado de " + n1 + " + " + n2 + " é: " + (n1 + n2));
+console.log("O resultado de " + n1 + " - " + n2 + " é: " + (n1 - n2));
+console.log("O resultado de " + n1 + " x " + n2 + " é: " + (n1 * n2));
+console.log("O resultado de " + n1 + " ÷ " + n2 + " é: " + (n1 / n2));
