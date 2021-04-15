c1aa74bc317bd8dbdf5c61ba52105695f72492bc
Giulia Nascimento <m0mprogramm3r@gmail.com>
2021-04-15 15:25:06 -0300

    Banco de Dados

---
+++ Banco de Dados/trabalhinho.sql
@@ -0,0 +1,5 @@
+"codigo","nome","salario","cnpj","nome-2","cpf","funcao","cargo"
+1,"coxinha",1.239,"39846279387","Giulia","62345523498","vendedor","gerente"
+2,"croquete",2.837,"87376429","Ana","98734877689","caixa","supervisor"
+3,"joelho",2.832,"837462565","Larissa","89734509832","faxina","supervisor"
+4,"kibe",1.763,"8374720897","Day","98743209832","recepção","gerente geral"
